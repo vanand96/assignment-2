9faccc52330bd4450faf089186270a13e54c8bbe
avadavelli <anand.vadavelli@viasat.com>
2021-02-14 15:56:59 -0800

    Updated project to display inventory management

--- public/App.js
+++ public/App.js
@@ -1,21 +1,197 @@
-const element = /*#__PURE__*/React.createElement("div", {
-  className: "grid-container"
-}, /*#__PURE__*/React.createElement("div", {
-  className: "user"
-}, /*#__PURE__*/React.createElement("h1", null, " ANAND VADAVELLI ")), /*#__PURE__*/React.createElement("div", {
-  className: "picture"
-}, /*#__PURE__*/React.createElement("img", {
-  src: "/img/Headshot.JPG",
-  alt: "Headshot",
-  width: "300",
-  height: "300"
-})), /*#__PURE__*/React.createElement("div", {
-  className: "description"
-}, "Working as a Test Automation Engineer at Viasat and pursuing Masters in computer science at SDSU. I am taking this course to pursue my carrier as a full stack engineer."), /*#__PURE__*/React.createElement("div", {
-  className: "code"
-}, /*#__PURE__*/React.createElement("a", {
-  href: "https://github.com/vanand96",
-  target: "blank",
-  className: "button"
-}, "VIEW MY GITHUB REPO")));
+"use strict";
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var initialProducts = [];
+
+function ProductRow(props) {
+  var product = props.Product;
+  return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, product.id), /*#__PURE__*/React.createElement("td", null, product.productname), /*#__PURE__*/React.createElement("td", null, product.price), /*#__PURE__*/React.createElement("td", null, product.category), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("a", {
+    href: product.url,
+    target: "_blank"
+  }, "View")));
+}
+
+function ProductTable(props) {
+  var productRows = props.products.map(function (Product) {
+    return /*#__PURE__*/React.createElement(ProductRow, {
+      key: Product.id,
+      Product: Product
+    });
+  });
+  return /*#__PURE__*/React.createElement("table", {
+    className: "bordered-table"
+  }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "ID"), /*#__PURE__*/React.createElement("th", null, "Category"), /*#__PURE__*/React.createElement("th", null, "Price"), /*#__PURE__*/React.createElement("th", null, "Product Name"), /*#__PURE__*/React.createElement("th", null, "Image URL"))), /*#__PURE__*/React.createElement("tbody", null, productRows));
+}
+
+var ProductAdd = /*#__PURE__*/function (_React$Component) {
+  _inherits(ProductAdd, _React$Component);
+
+  var _super = _createSuper(ProductAdd);
+
+  function ProductAdd() {
+    var _this;
+
+    _classCallCheck(this, ProductAdd);
+
+    _this = _super.call(this);
+    _this.handleSubmit = _this.handleSubmit.bind(_assertThisInitialized(_this));
+    return _this;
+  }
+
+  _createClass(ProductAdd, [{
+    key: "handleSubmit",
+    value: function handleSubmit(e) {
+      e.preventDefault();
+      var form = document.forms.productAdd;
+      var product = {
+        category: form.products.value,
+        price: form.PricePerUnit.value,
+        productname: form.ProductName.value,
+        url: form.ImageUrl.value
+      };
+      this.props.createProduct(product);
+      form.products.value = "";
+      form.PricePerUnit.value = "$";
+      form.ProductName.value = "";
+      form.ImageUrl.value = "";
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/React.createElement("form", {
+        name: "productAdd",
+        onSubmit: this.handleSubmit
+      }, /*#__PURE__*/React.createElement("label", {
+        htmlFor: "products"
+      }, "Category"), /*#__PURE__*/React.createElement("select", {
+        name: "products",
+        id: "products"
+      }, /*#__PURE__*/React.createElement("option", {
+        value: "empty"
+      }), /*#__PURE__*/React.createElement("option", {
+        value: "Shirts"
+      }, "Shirts"), /*#__PURE__*/React.createElement("option", {
+        value: "Accessories"
+      }, "Accessories"), /*#__PURE__*/React.createElement("option", {
+        value: "Jeans"
+      }, "Jeans"), /*#__PURE__*/React.createElement("option", {
+        value: "Jackets"
+      }, "Jackets"), /*#__PURE__*/React.createElement("option", {
+        value: "Sweaters"
+      }, "Sweaters")), /*#__PURE__*/React.createElement("label", {
+        htmlFor: "PricePerUnit"
+      }, "Price Per Unit"), /*#__PURE__*/React.createElement("input", {
+        type: "text",
+        name: "PricePerUnit",
+        id: "PricePerUnit",
+        defaultValue: "$"
+      }), /*#__PURE__*/React.createElement("label", {
+        htmlFor: "productName"
+      }, "Product Name"), /*#__PURE__*/React.createElement("input", {
+        type: "text",
+        name: "ProductName",
+        id: "ProductName",
+        placeholder: "Enter product name"
+      }), /*#__PURE__*/React.createElement("label", {
+        htmlFor: "ImageUrl"
+      }, "Image URL"), /*#__PURE__*/React.createElement("input", {
+        type: "text",
+        name: "ImageUrl",
+        id: "ImageUrl",
+        placeholder: "ImageUrl"
+      }), /*#__PURE__*/React.createElement("input", {
+        type: "submit",
+        onClick: cleardollar
+      }));
+
+      function cleardollar() {
+        var input = document.getElementById("PricePerUnit");
+        input.value = input.value.replace(/\$/g, "");
+      }
+    }
+  }]);
+
+  return ProductAdd;
+}(React.Component);
+
+var ProductList = /*#__PURE__*/function (_React$Component2) {
+  _inherits(ProductList, _React$Component2);
+
+  var _super2 = _createSuper(ProductList);
+
+  function ProductList() {
+    var _this2;
+
+    _classCallCheck(this, ProductList);
+
+    _this2 = _super2.call(this);
+    _this2.state = {
+      products: []
+    };
+    _this2.createProduct = _this2.createProduct.bind(_assertThisInitialized(_this2));
+    return _this2;
+  }
+
+  _createClass(ProductList, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      this.loadData();
+    }
+  }, {
+    key: "loadData",
+    value: function loadData() {
+      var _this3 = this;
+
+      setTimeout(function () {
+        _this3.setState({
+          products: initialProducts
+        });
+      }, 500);
+    }
+  }, {
+    key: "createProduct",
+    value: function createProduct(product) {
+      product.id = this.state.products.length + 1;
+      var newProductList = this.state.products.slice();
+      newProductList.push(product);
+      this.setState({
+        products: newProductList
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "My Company Inventory"), /*#__PURE__*/React.createElement("div", null, "Showing all available products"), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(ProductTable, {
+        products: this.state.products
+      }), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("br", null), "Add a new product to inventory"), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(ProductAdd, {
+        createProduct: this.createProduct
+      }));
+    }
+  }]);
+
+  return ProductList;
+}(React.Component);
+
+var element = /*#__PURE__*/React.createElement(ProductList, null);
 ReactDOM.render(element, document.getElementById("content"));
